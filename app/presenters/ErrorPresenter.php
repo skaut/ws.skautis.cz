9342b7151ec3a46c14f8dc080f20f389b449bb39
sinacek <sinacek@gmail.com>
2013-10-16 14:46:43 +0200

    oprava kontroly excepttion pro skautis

--- app/presenters/ErrorPresenter.php
+++ app/presenters/ErrorPresenter.php
@@ -11,15 +11,15 @@ class ErrorPresenter extends Nette\Application\UI\Presenter {
         } elseif ($exception instanceof BadRequestException) {
             $code = $exception->getCode();
             $this->setView(in_array($code, array(403, 404, 405, 410, 500)) ? $code : '4xx'); // load template 403.latte or 404.latte or ... 4xx.latte
-        } elseif ($exception instanceof SkautIS_Exception) {
-            Debugger::log($exception, Debugger::WARNING); // and log exception
-            $this->setView('SkautIS');
-            $this->template->ex = $exception;
-        } elseif ($exception instanceof SkautIS_AuthenticationException) {//vypršelo přihlášení do SkautISu
+        } elseif ($exception instanceof SkautIS\Exception\AuthenticationException) {//vypršelo přihlášení do SkautISu
             $this->user->logout(TRUE);
             $this->flashMessage($exception->getMessage() != "" ? $exception->getMessage() : "Vypršelo přihlášení do SkautISu" , "danger");
             $backlink = isset($exception->backlink) ? $exception->backlink : NULL;
             $this->redirect(":Default:", array("backlink" => $backlink));
+        } elseif ($exception instanceof SkautIS\Exception\BaseException) {
+            Debugger::log($exception, Debugger::WARNING); // and log exception
+            $this->setView('SkautIS');
+            $this->template->ex = $exception;
         } else {
             $this->setView('500'); // load template 500.latte
             Debugger::log($exception, Debugger::ERROR); // and log exception
